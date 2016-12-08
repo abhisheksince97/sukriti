a35afe0b9babe119ceb573094c758eac32e165bc
Amanpreet Singh <apsdehal@gmail.com>
2016-12-08 12:12:06 -0500

    Updated changes

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -329,7 +329,7 @@ if (typeof jQuery === 'undefined') {
   Carousel.TRANSITION_DURATION = 800
 
   Carousel.DEFAULTS = {
-    interval: 4000,
+    interval: 5000,
     pause: 'hover',
     wrap: true,
     keyboard: true
